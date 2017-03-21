48109af4381d0d284761f965dd5b6dfc6dce555d
Diego Rodrigues <diego.mrodrigues@outlook.com>
2017-03-21 08:17:57 -0300

    The route responsible for getting the external stimuli and feeding it to the agent. Then, getting and format its reaction

--- src/natmed.py
+++ src/natmed.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+from src.agent import agent
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Natmed Knowledge Based Agent API v1.0.0"
+
+@app.route("/perceive", methods=['POST'])
+def perceive():
+    """ The Agent's reaction API to external enviromental stimuli.
+    """
+    stimuli = request.json
+    action = agent.perceive(stimuli)
+    return action.to_json()
