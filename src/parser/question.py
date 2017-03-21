ac2277d1c5e8f896d1f5aae18b9427e546a6d1b0
Diego Rodrigues <diego.mrodrigues@outlook.com>
2017-03-21 09:00:42 -0300

    Remove the entiity list and added the check for the entiities.pickle

--- src/parser/question.py
+++ src/parser/question.py
@@ -1,28 +1,20 @@
 import pandas as pd
 import numpy as np
+import os.path as path
 import pickle
 import re
 import difflib
 
+if not path.isfile('./assets/dumps/entities.pickle'):
+    raise Exception("Entities pickle file not found!")
+
 def compile(question):
     return "Compiled Question"
 
-ENTITY_LIST = [
-    'Medicine',
-    'Disease',
-    'Food',
-    'Context',
-    'HerbSuplement',
-    'LaboratoryTest',
-    'Pharmacokinetics',
-    'ScientificName',
-    'Synonymous'
-]
-
 QUESTIONS = [
-    # What Is Question
+    # What Is Questions
     ('What is ([A-Z].*)\?', 'WHAT_IS'),
-    # Simple Relation Question
+    # Simple Relation Questions
     ('What is the relation between ([A-Z].*) and ([A-Z].*)\?', 'SIMPLE_RELATION'),
     ('How are ([A-Z].*) and ([A-Z].*) related\?', 'SIMPLE_RELATION'),
     ('Is ([A-Z].*) related to ([A-Z].*)\?', 'SIMPLE_RELATION')
