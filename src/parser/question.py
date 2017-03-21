801fbeb96cebef2a9804c818c9a5018ae9d5f16c
Diego Rodrigues <diego.mrodrigues@outlook.com>
2017-03-21 08:56:00 -0300

    Adding the questions global array

--- src/parser/question.py
+++ src/parser/question.py
@@ -18,3 +18,12 @@ ENTITY_LIST = [
     'ScientificName',
     'Synonymous'
 ]
+
+QUESTIONS = [
+    # What Is Question
+    ('What is ([A-Z].*)\?', 'WHAT_IS'),
+    # Simple Relation Question
+    ('What is the relation between ([A-Z].*) and ([A-Z].*)\?', 'SIMPLE_RELATION'),
+    ('How are ([A-Z].*) and ([A-Z].*) related\?', 'SIMPLE_RELATION'),
+    ('Is ([A-Z].*) related to ([A-Z].*)\?', 'SIMPLE_RELATION')
+]
