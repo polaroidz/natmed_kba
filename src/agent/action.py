3b9405a7504c3b60b9ee1a27f8d0bbb72c5a2a97
Diego Rodrigues <diego.mrodrigues@outlook.com>
2017-03-22 08:24:59 -0300

    Defining the action interface and implementing the basic answer concrete

--- src/agent/action.py
+++ src/agent/action.py
@@ -1,8 +1,21 @@
 import json
 
 class Action(object):
+    def __init__(self):
+        pass
+
+    def act(self):
+        raise NotImplementedError("The process has not been overwriten.")
+      
+    def to_json(self):
+        raise NotImplementedError("The to_json has not been overwriten.")
+
+class AnswerAction(Action):
     def __init__(self, question):
         self.question = question
-    
+
+    def act(self):
+        pass
+
     def to_json(self):
-        return json.dumps(self.question)
+        return json.dumps(self.question, indent=True)
